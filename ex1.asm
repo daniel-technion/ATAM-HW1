98820f602d1991d54438733fbee8f33a792c53be
danielperezz <Daniel06715@gmail.com>
2023-04-19 17:51:37 +0300

    ex1

--- ex1.asm
+++ ex1.asm
@@ -3,3 +3,20 @@
 .section .text
 _start:
 #your code here
+            movq (num), %rax
+            movq $0, %rcx #counter (to 64)
+            movq $0, %rdx #counts 1's
+
+            
+loop_HW1:   movq $1, %rbx
+            shll %cl, %rbx
+            and %rax, %rbx 
+            cmp %rbx, $0 
+            jz zero_HW1 #the bit we currently check is 0, skip counter inc
+            inc %rdx
+zero_HW1:   inc %rcx
+            cmp $64, %rcx
+            jne loop_HW1
+
+            movb %rdx (Bool) 
+
