bbe0eaa3357de059413eb4bf44085c6221cea296
Avia Avraham <avia.avraham@gmail.com>
2023-04-28 17:39:23 +0300

    need to fix 4!!

--- ex2.asm
+++ ex2.asm
@@ -16,7 +16,7 @@ _start:
 srcToDest_HW1:
         movq $0, %r11 #counter
         cmp %r8, %r9
-        #jg reverse_HW1 #r9 > r8, writing to dest might ruin src, use modified loop
+        jg reverse_HW1 #r9 > r8, writing to dest might ruin src, use modified loop
         #todo need to fix reverse
 loop_HW1:
         movb (%r8, %r11, 1), %r12b
@@ -28,12 +28,13 @@ loop_HW1:
 
 reverse_HW1:
         movl %r10d, %r11d #counter
+        dec %r11d #i think it's needed to avoid accessing out of bounds thing - like arr[n-1] instead of arr[n]
 loop_reverse_HW1:
         movb (%r8, %r11, 1), %r12b
         movb %r12b, (%r9, %r11, 1)
         dec %r11d
         cmp $0, %r11d
-        jne loop_reverse_HW1
+        jge loop_reverse_HW1 #didnt work with r11d != 0, because we need to also move that first bit, this should work
         jmp end_HW1
 
 end_HW1:
