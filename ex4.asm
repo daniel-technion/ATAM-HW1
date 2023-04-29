b46b78c1f14b1068d9d6005753e25f0f707eeaaa
Avia Avraham <avia.avraham@gmail.com>
2023-04-29 22:35:07 +0300

    4 works fine!!!!!

--- ex4.asm
+++ ex4.asm
@@ -16,56 +16,123 @@ _start:
     movq $0, %rdx   #pointer to value's node previous
 
 loop_HW1:
-    movq 4(%rdi), %rcx #rcx is next node
-    cmpq $0, %rcx
-    je continue_HW1
-
-    cmpl (%rcx), %r8d
-    je save_node_HW1
+    cmpl (%rdi), %r8d
+    je save_node_HW1 #current node = value
     jmp continue_HW1
 save_node_HW1:
     cmp $0, %rdx
     jne continue_HW1
-    movq %rdi, %rdx
+    movq %rcx, %rdx #save prev addrs
 continue_HW1:
-    cmpq %rcx, %r9
+    cmpq %rdi, %r9 #check if it's source
     je save_source_parent_HW1
     jmp continue2_HW1
 save_source_parent_HW1:
     cmp $0, %rbx
     jne continue2_HW1
-    movq %rdi, %rbx
+    movq %rcx, %rbx
 continue2_HW1:
+    movq %rdi, %rcx
     movq 4(%rdi), %rdi    #we jump 8 because each int is 4 bytes, rdi = rdi.next
     cmp $0, %rdi      #will jump as long as rdi != 0
     jne loop_HW1
-    #jmp end_HW1
 
-swap_HW1:
-    #rdi points to the node
-    cmp $0, %rdx
-    jne swap_previous_val_HW1
-    movq head(%rip), %rdi
-    cmpl (%rdi), %r8d
-    jne end_HW1 #not found
-    movq %rdi, %rdx
-    jmp swap_src_to_val #
 
-swap_previous_val_HW1:
-    movq %r9, 4(%rdx)
+
+#=======================================================================================
+
+
+    cmpq $0, %rbx   #pointer to Source's previous
+    je sourceIsRoot
+    cmpq $0, %rdx   #pointer to value's node previous
+    je valIsRootOrMissing
+    #neither are 0
+
+normal_swap:
+    movq 4(%rdx), %r10 #backup A.next
+    movq 4(%rbx), %r11
+    movq %r11, 4(%rdx)
+    movq %r10, 4(%rbx) #swaps the 'previous' arrows
+
+    #registers have no meaning anymore so why not
     movq 4(%rdx), %rdx
-swap_src_to_val:
-    movq 4(%rdx), %rax
-    movq 4(%r9), %r12
-    movq %r12, 4(%rdx) #r12
+    movq 4(%rbx), %rbx
 
-    cmp $0, %rbx
-    je swap_val_to_src
-swap2_HW1:
-    movq %rdx, 4(%rbx)
-swap_val_to_src:
-    movq %rax, 4(%r9)
-    #movq %rax, 4(%rdi)
-jmp end_HW1
+    movq 4(%rdx), %r10 #backup A.next
+    movq 4(%rbx), %r11
+    movq %r11, 4(%rdx)
+    movq %r10, 4(%rbx) #swaps the 'next' arrows
+
+    jmp end_HW1
+
+sourceIsRoot: #rbx == 0!!!
+/*
+%rbx   #Source's previous
+%rdx   #value's previous
+    *set source-next to be val's next
+    *set val-next to be source's next
+    *set val-previous to be source
+*/
+    cmpq $0, %rdx
+    je valIsRootOrMissingAndBothAreZero
+
+    movq Source(%rip), %rbx #rbx is source
+    movq 4(%rdx), %r11 #backup previous-node-value's next
+    movq %rbx, 4(%rdx) #previous value's next -> source
+
+    movq %r11, %rdx #now rdx is previous value's next, aka value
+    #movq 4(%r11), %rdx #this is completely wrong as it sets rdx to be r11's next (value's next)
+    #important? make head point to value
+
+    movq %rdx, head(%rip)
+
+    #movq $node_5, head(%rip)
+
+    movq 4(%rdx), %r10 #backup A.next
+    movq 4(%rbx), %r11
+    movq %r11, 4(%rdx)
+    movq %r10, 4(%rbx) #swaps the 'next' arrows
+
+    jmp end_HW1
+
+valIsRootOrMissingAndBothAreZero:
+    #both are 0, either source is value or value n/a
+    movq Source(%rip), %rdx #rdx = root
+    cmpl %r8d, (%rdx)
+    jne end_HW1 #not found
+    #val is root AND source is root, no need to swap anything
+    jmp end_HW1
+
+valIsRootOrMissing: #rdx = 0, rbx != 0
+
+    movq head(%rip), %rdx #rdx = root
+    cmpl %r8d, (%rdx) #check root value vs given value
+    jne end_HW1 #not found
+
+    #val is root, source isn't
+/*
+%rbx   #Source's previous
+%rdx   #value's previous
+    if val is root: (here it is)
+    *set val-next to be source's next
+    *set source-next to be val's next
+    *set source-previous to be val
+*/
+    movq 4(%rbx), %r11 #backup previous source's next
+    movq %rdx, 4(%rbx) #now source's prev -> val
+
+    movq %r11, %rbx #now rbx is source, aka previous source's next, rdx (val) is source no need to change
+
+    #important! make head point to source
+    movq %rbx, head(%rip)
+
+    movq 4(%rbx), %r10 #backup source.next
+    movq 4(%rdx), %r11 #backup val.next (second node overall)
+    movq %r11, 4(%rbx)
+    movq %r10, 4(%rdx) #swaps the 'next' arrows
+
+    jmp end_HW1
+
+
 
 end_HW1:
